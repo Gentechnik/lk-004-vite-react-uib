e097aa2c56b5080aeb2678c40d31b9fc3615d7be
Lukas Karberg <deaderlp@googlemail.com>
2023-07-11 14:51:28 +0200

    Lukas: Fixed Error in PageWelcome

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -6,16 +6,16 @@ export const PageWelcome = () => {
           <h1>Hi and welcome, to my first shot at a React Page!</h1>
           <p>down below are some of the Projects I worked on up until now</p>
         </section>
-        <section class="welcome_page_container">
-          <div class="welcome_page_item">
+        <section className="welcome_page_container">
+          <div className="welcome_page_item">
             <p>Item 1</p>
             <img src="https://placehold.co/600x400" alt="Placeholder image" />
           </div>
-          <div class="welcome_page_item">
+          <div className="welcome_page_item">
             <p>Item 2</p>
             <img src="https://placehold.co/600x400" alt="Placeholder image" />
           </div>
-          <div class="welcome_page_item">
+          <div className="welcome_page_item">
             <p>Item 3</p>
             <img src="https://placehold.co/600x400" alt="Placeholder image" />
           </div>
