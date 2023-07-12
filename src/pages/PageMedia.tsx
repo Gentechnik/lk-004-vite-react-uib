9aa70c1d932b2776320a629cc5a884de532b4524
Lukas Karberg <deaderlp@googlemail.com>
2023-07-12 16:18:47 +0200

    Lukas: Edited PageMedia, added Images and SCSS

--- src/pages/PageMedia.tsx
+++ src/pages/PageMedia.tsx
@@ -1,7 +1,266 @@
 export const PageMedia = () => {
   return (
     <>
-      <p>This is the media page.</p>
+      <main className="media_page">
+        <h2>
+          This is an overview of media I enjoyed over the years and generally
+          can recommend.
+        </h2>
+        <section className="media_page_music">
+          <h3>MUSIC</h3>
+          <div className="content_card">
+            <img
+              src="images/music/runners-high-pillows.jpg"
+              alt="Cover of the Album 'Runners High' from the Pillows"
+              title="Cover of the Album 'Runners High' from the Pillows"
+            />
+            <div className="content_card_text">
+              <h4>the Pillows - Runners High</h4>
+              <p>
+                Nectar is the second studio album by Japanese singer-songwriter
+                Joji, released on 25 September 2020 via 88rising.
+              </p>
+            </div>
+          </div>
+          <div className="content_card">
+            <img src="images/music/gorillaz-demon-days.png" alt="#" />
+            <div className="content_card_text">
+              <h4>Gorillaz - Demon Days</h4>
+              <p>
+                Demon Days is the second studio album by British virtual band
+                Gorillaz. It was first released on 11 May 2005 in Japan and on
+                23 May 2005 in the United Kingdom by Parlophone and in the
+                United States by Virgin Records.
+              </p>
+            </div>
+          </div>
+          <div className="content_card">
+            <img
+              src="images/music/joji-nectar.png"
+              alt="Cover of the Album 'Nectar' from Joji"
+              title="Cover of the Album 'Nectar' from Joji"
+            />
+            <div className="content_card_text">
+              <h4>Joji - Nectar</h4>
+              <p>
+                Nectar is the second studio album by Japanese singer-songwriter
+                Joji, released on 25 September 2020 via 88rising.
+              </p>
+            </div>
+          </div>
+          <div className="content_card">
+            <img
+              src="images/music/nier-replicant-ost.jpg"
+              alt="Cover of the Original Soundtrack of the Game 'Nier Replicant'"
+              title="Cover of the Original Soundtrack of the Game 'Nier Replicant'"
+            />
+            <div className="content_card_text">
+              <h4>NieR Gestalt & Replicant - Videogame OST</h4>
+              <p>
+                NieR Gestalt & Replicant Original Soundtrack Soundtrack album by
+                Keigo Hoashi, Keiichi Okabe, and SQUARE ENIX MUSIC.
+              </p>
+            </div>
+          </div>
+          <div className="content_card">
+            <img
+              src="images/music/oliver-tree-life-goes-on.jpg"
+              alt="Cover of the Album 'Ugly is Beautiful' from Oliver Tree"
+              title="Cover of the Album 'Ugly is Beautiful' from Oliver Tree"
+            />
+            <div className="content_card_text">
+              <h4>Oliver Tree - Ugly Is Beautiful</h4>
+              <p>
+                Ugly Is Beautiful is the debut major-label studio album by
+                American singer-songwriter Oliver Tree.
+              </p>
+            </div>
+          </div>
+          <div className="content_card">
+            <img
+              src="images/music/made-in-abyss.png"
+              alt="Cover of the Original Soundtrack of the Series 'Made in Abyss'"
+              title="Cover of the Original Soundtrack of the Series 'Made in Abyss'"
+            />
+            <div className="content_card_text">
+              <h4>Made in Abyss - Anime OST</h4>
+              <p>
+                A young girl finds and befriends a humanoid robot and descends
+                with him into the titular Abyss in search of her mother.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className="media_page_games">
+          <h3>Games</h3>
+          <div className="content_card">
+            <img
+              src="images/games/bioshock.jpg"
+              alt="Cover of the Game 'Bioshock"
+              title="Cover of the Game 'Bioshock"
+            />
+            <div className="content_card_text">
+              <h4>Bioshock</h4>
+              <p>
+                In 1960, a lone survivor of a plane crash named Jack discovers
+                an abandoned underwater utopia, only to find out that the
+                mystery behind its creation is much more sinister than he first
+                believed.
+              </p>
+            </div>
+          </div>
+          <div className="content_card">
+            <img
+              src="images/games/inscryption.jpg"
+              alt="Cover of the Game 'Inscryption'"
+              title="Cover of the Game 'Inscryption'"
+            />
+            <div className="content_card_text">
+              <h4>Inscryption</h4>
+              <p>
+                Inscryption is an inky black card-based odyssey that blends the
+                deckbuilding roguelike, escape-room style puzzles, and
+                psychological horror into a blood-laced smoothie. Darker still
+                are the secrets inscrybed upon the cards...
+              </p>
+            </div>
+          </div>
+          <div className="content_card">
+            <img
+              src="images/games/monster-hunter-rise.png"
+              alt="Cover of the game 'Monsterhunter Rise"
+              title="Cover of the game 'Monsterhunter Rise"
+            />
+            <div className="content_card_text">
+              <h4>Monsterhunter Rise</h4>
+              <p>
+                As with previous Monster Hunter titles, Monster Hunter Rise has
+                the player take the role of a Hunter, tasked with slaying or
+                capturing large monsters using a variety of weapons, tools, and
+                environmental features to damage and weaken them while surviving
+                their attacks.
+              </p>
+            </div>
+          </div>
+          <div className="content_card">
+            <img
+              src="images/games/nier-replicant.jpg"
+              alt="Cover from the game 'NieR Replicant ver.1.22474487139...'"
+              title="Cover from the game 'NieR Replicant ver.1.22474487139...'"
+            />
+            <div className="content_card_text">
+              <h4>NieR Replicant ver.1.22474487139...</h4>
+              <p>
+                Set in the year 3465, the game follows Nier as he tries to save
+                his sister Yonah from a fatal disease known as the Black Scrawl.
+                Fighting alongside him are the magical book Grimoire Weiss, the
+                possessed foul mouth Kainé and the cursed immortal child Emil
+                who petrifies people.
+              </p>
+            </div>
+          </div>
+          <div className="content_card">
+            <img
+              src="images/games/discroom.png"
+              alt="Cover of the Game 'Discroom'"
+              title="Cover of the Game 'Discroom'"
+            />
+            <div className="content_card_text">
+              <h4>Discroom</h4>
+              <p>
+                Disc Room is a 2020 action puzzle video game in which the player
+                navigates a series of labyrinthine rooms while avoiding bullet
+                hell-style projectiles. Each room has its own patterns but is
+                not predictable between rooms.
+              </p>
+            </div>
+          </div>
+          <div className="content_card">
+            <img
+              src="images/games/battlebit-remastered.png"
+              alt="Cover from the Game 'Battle Bit Remastered'"
+              title="Cover from the Game 'Battle Bit Remastered'"
+            />
+            <div className="content_card_text">
+              <h4>Battle Bit Remastered</h4>
+              <p>
+                BattleBit Remastered is a low-poly, massive multiplayer FPS,
+                supporting 254 players per server. Battle on a near-fully
+                destructible map with various vehicles!
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className="media_page_manga">
+          <h3>Manga</h3>
+          <div className="content_card">
+            <img
+              src="https://placehold.co/600x400/EEE/31343C"
+              alt="Cover of the Game 'Bioshock"
+              title="Cover of the Game 'Bioshock"
+            />
+            <div className="content_card_text">
+              <h4>Placeholder</h4>
+              <p>placeholder</p>
+            </div>
+          </div>
+          <div className="content_card">
+            <img
+              src="https://placehold.co/600x400/EEE/31343C"
+              alt="Cover of the Game 'Bioshock"
+              title="Cover of the Game 'Bioshock"
+            />
+            <div className="content_card_text">
+              <h4>Placeholder</h4>
+              <p>placeholder</p>
+            </div>
+          </div>
+          <div className="content_card">
+            <img
+              src="https://placehold.co/600x400/EEE/31343C"
+              alt="Cover of the Game 'Bioshock"
+              title="Cover of the Game 'Bioshock"
+            />
+            <div className="content_card_text">
+              <h4>Placeholder</h4>
+              <p>placeholder</p>
+            </div>
+          </div>
+          <div className="content_card">
+            <img
+              src="https://placehold.co/600x400/EEE/31343C"
+              alt="Cover of the Game 'Bioshock"
+              title="Cover of the Game 'Bioshock"
+            />
+            <div className="content_card_text">
+              <h4>Placeholder</h4>
+              <p>placeholder</p>
+            </div>
+          </div>
+          <div className="content_card">
+            <img
+              src="https://placehold.co/600x400/EEE/31343C"
+              alt="Cover of the Game 'Bioshock"
+              title="Cover of the Game 'Bioshock"
+            />
+            <div className="content_card_text">
+              <h4>Placeholder</h4>
+              <p>placeholder</p>
+            </div>
+          </div>
+          <div className="content_card">
+            <img
+              src="https://placehold.co/600x400/EEE/31343C"
+              alt="Cover of the Game 'Bioshock"
+              title="Cover of the Game 'Bioshock"
+            />
+            <div className="content_card_text">
+              <h4>Placeholder</h4>
+              <p>placeholder</p>
+            </div>
+          </div>
+        </section>
+      </main>
     </>
   );
 };
--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,29 +1,34 @@
 export const PageWelcome = () => {
   return (
     <>
-      <main>
+      <main className="welcome_page">
         <section className="welcome_page_heading">
           <h1>Hi and welcome, to my first shot at a React Page!</h1>
           <p>down below are some of the Projects I worked on up until now</p>
         </section>
         <section className="welcome_page_container">
           <div className="welcome_page_item">
-            <a href="#">My First HTML/CSS Site</a>
+            <a href="https://gentechnik.github.io/cautious-umbrella/">
+              My First HTML/CSS Site
+            </a>
             <p>
               This Site was created by me towards the end of my
               Orientationcourse.
             </p>
-            <img src="https://placehold.co/600x400" alt="Placeholder image" />
+            <img
+              src="images/my-first-html-css-site.png"
+              alt="Image of my first HMTL/CSS Site"
+            />
           </div>
           <div className="welcome_page_item">
             <a href="#">New Project #1</a>
-            <p>Item 1</p>
-            <img src="https://placehold.co/600x400" alt="Placeholder image" />
+            <p>Coming Soon!</p>
+            <img src="images/placeholder.png" alt="Placeholder image" />
           </div>
           <div className="welcome_page_item">
             <a href="#">New Project #2</a>
             <p>Coming Soon!</p>
-            <img src="https://placehold.co/600x400" alt="Placeholder image" />
+            <img src="images/placeholder.png" alt="Placeholder image" />
           </div>
         </section>
       </main>
