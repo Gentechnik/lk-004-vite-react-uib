f2223d41a44165f7ecde4717759d1060ad34a02f
Lukas Karberg <deaderlp@googlemail.com>
2023-07-13 18:00:09 +0200

    Lukas: Added Animations and React Icons

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { PageWelcome } from "./pages/PageWelcome";
 import { PageMedia } from "./pages/PageMedia";
 import { PageAbout } from "./pages/PageAbout";
 import { PageNotFound } from "./pages/PageNotFound";
+import { AiOutlineTwitter, AiFillGithub } from "react-icons/ai";
+import { FaRandom } from "react-icons/fa";
 
 function App() {
   return (
@@ -30,13 +32,34 @@ function App() {
           </h3>
           <ul>
             <li>
-              <a href="#"></a>
+              <a
+                href="https://github.com/Gentechnik"
+                title="My Github Page"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
             </li>
             <li>
-              <a href="#"></a>
+              <a
+                href="https://twitter.com/mischiefanimals/status/1624898107283591168"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="a funny Cat video"
+              >
+                <FaRandom />
+              </a>
             </li>
             <li>
-              <a href="#"></a>
+              <a
+                href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="you know the rules"
+              >
+                <AiOutlineTwitter />
+              </a>
             </li>
           </ul>
         </footer>
