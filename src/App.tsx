e6a5c495d8de1aca27b88a314e152a03b711a03e
Lukas Karberg <deaderlp@googlemail.com>
2023-10-09 13:59:59 +0200

    Converted Footer into a Component

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,7 @@ import { PageMedia } from "./pages/PageMedia";
 import { PageAbout } from "./pages/PageAbout";
 import { PageRandom } from "./pages/PageRandom";
 import { PageNotFound } from "./pages/PageNotFound";
-import { AiOutlineTwitter, AiFillGithub } from "react-icons/ai";
-import { FaRandom } from "react-icons/fa";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -18,7 +17,6 @@ function App() {
           <NavLink to="/random">Random</NavLink>
           <NavLink to="/about">About</NavLink>
         </nav>
-
         <Routes>
           <Route path="/welcome" element={<PageWelcome />} />
           <Route path="/media" element={<PageMedia />} />
@@ -27,45 +25,7 @@ function App() {
           <Route path="/" element={<Navigate to="/welcome" replace />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
-
-        <footer>
-          <h3>
-            If you want to reach out to me, down below are my Github and 2
-            random links
-          </h3>
-          <ul>
-            <li>
-              <a
-                href="https://github.com/Gentechnik"
-                title="My Github Page"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <AiFillGithub />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://twitter.com/mischiefanimals/status/1624898107283591168"
-                target="_blank"
-                rel="noopener noreferrer"
-                title="a funny Cat video"
-              >
-                <FaRandom />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-                target="_blank"
-                rel="noopener noreferrer"
-                title="you know the rules"
-              >
-                <AiOutlineTwitter />
-              </a>
-            </li>
-          </ul>
-        </footer>
+        <Footer />
       </div>
     </>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,47 @@
+import { AiOutlineTwitter, AiFillGithub } from "react-icons/ai";
+import { FaRandom } from "react-icons/fa";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <h3>
+        If you want to reach out to me, down below are my Github and 2 random
+        links
+      </h3>
+      <ul>
+        <li>
+          <a
+            href="https://github.com/Gentechnik"
+            title="My Github Page"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AiFillGithub />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://twitter.com/mischiefanimals/status/1624898107283591168"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="a funny Cat video"
+          >
+            <FaRandom />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="you know the rules"
+          >
+            <AiOutlineTwitter />
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
